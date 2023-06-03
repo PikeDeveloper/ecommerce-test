1824cb2875b00c43c440d1d520aba46e3d7ce5e6
PikeDeveloper <cabelloenrique@hotmail.com>
2023-06-03 10:43:47 -0500

    inicio de pagina

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+export default function AboutPage() {
+    return (
+      <main className={styles.main}>
+       <h1>About Page</h1>
+      </main>
+    )
+  }
+  
